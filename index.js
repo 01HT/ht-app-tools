5cc8fc8dae380da3a5e7e4c91b294de57aa14349
maksimyurkov <yurkov.maksim.sergeevich@gmail.com>
2019-08-28 19:29:08 +0300

    add error handler

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const puppeteer = require("puppeteer");
 const fs = require("fs");
 const fetch = require("node-fetch");
 
-const url = "http://localhost:5000/aboutd";
+const url = "https://elements-app-dev.web.app/item/ims-user-list/2";
 const googleUserAgent = "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.96 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)";
 
 async function saveInFile(str) {
@@ -19,6 +19,16 @@ async function getInPuppeteer(url, userAgent) {
     headless: true
   });
   const page = await browser.newPage();
+
+  page.on("pageerror", function(err) {  
+    theTempValue = err.toString();
+    console.log("Page error: " + theTempValue); 
+  })
+
+  page.on("error", function (err) {  
+    theTempValue = err.toString();
+    console.log("Error: " + theTempValue); 
+  })
 
   await page.setUserAgent(userAgent);
 
